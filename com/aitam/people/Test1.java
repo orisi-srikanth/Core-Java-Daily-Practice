85d76dfeaed9fb57033039f9259edea82fa8787a
Orisi Srikanth <srikanthorisi62@gmail.com>
2026-09-07 21:56:45 +0530

    Add Test1.java to demonstrate Person and AadharaCard

---
+++ com/aitam/people/Test1.java
@@ -0,0 +1,9 @@
+package com.aitam.people;
+class Test1{
+public static void main(String[] args){
+Person obj=new Person("Ekshi",20,"S.Ekshita",5484552);
+AadharaCard obj1 = new AadharaCard("Thelidhu", 525525, "10-12-2005");
+obj.display();
+obj1.display();
+}
+} 
